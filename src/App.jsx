676089bafa6a800f083ea381a6fc622128462b20
VazquezItzell <itzell.vazquez@corebiz.ag>
2025-11-27 08:57:44 -0600

    chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Form from './components/form'
+import CsvTable from './components/Table'
+
+function App() {
+
+
+
+  return (
+    <>
+      <h1>Invenadro</h1>
+      <Form />
+      <CsvTable />
+
+    </>
+  )
+}
+
+export default App
